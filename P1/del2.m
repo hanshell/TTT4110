6ee692d53932a3d9d5b904db94546b694e8f85c7
Tormod Bjørnhaug <tormod@sinepost.no>
2014-03-11 11:10:34 +0100

    Updated freq2char description

--- P1/del2.m
+++ P1/del2.m
@@ -11,6 +11,9 @@ frequencies = [697, 770, 852, 941, 1209, 1336, 1477];
 Fs=8000;
 
 %% Frequency to character conversion
+% The frequency to character convertion is added as a separate function
+% called freq2char(). The function takes inn the sum of two frequencies and
+% returns the corresponding character.
 %
-%
+% For more information: freq2char.m
 
